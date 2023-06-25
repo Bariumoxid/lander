6598b3316bc6a0a52a09d964e6d3ec1662622f77
Bariumoxid <andrewbao0408@gmail.com>
2023-06-25 15:23:23 +0800

    v2.0

--- spring.cpp
+++ spring.cpp
@@ -1,11 +1,32 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <ctime>
 
 using namespace std;
 
-int main() {
+//Findings
+/* (ALL run without debug mode on)
+For the Euler simulation, dt=0.00001 
+    For C++ x64
+        Under Debug mode (no optimization): 3.31s 
 
+        Under Release mode (with optimization):0.309s
+
+    For C++ x86
+        Under Debug mode (no optimization): 8.5 s
+
+        Under Release mode (with optimization): 0.38s
+
+    #Release mode, the compilation with optimization is much faster than the debug mode, without optimization
+
+    For python: 
+*/
+
+int main() {
+    long i = 10000000L;
+    clock_t start, end;
+    start = clock();
   // declare variables
   double m, k, x, v, t_max, dt, t, a;
   vector<double> t_list, x_list, v_list;
@@ -18,22 +39,24 @@ int main() {
 
   // simulation time and timestep
   t_max = 100;
-  dt = 0.1;
+  dt = 0.00001;
 
   // Euler integration
   for (t = 0; t <= t_max; t = t + dt) {
 
-    // append current state to trajectories
-    t_list.push_back(t);
-    x_list.push_back(x);
-    v_list.push_back(v);
+      // append current state to trajectories
+      t_list.push_back(t);
+      x_list.push_back(x);
+      v_list.push_back(v);
 
-    // calculate new position and velocity
-    a = -k * x / m;
-    x = x + dt * v;
-    v = v + dt * a;
-
+      // calculate new position and velocity
+      a = -k * x / m;
+      x = x + dt * v;
+      v = v + dt * a;
   }
+  end = clock();
+  printf("Time: %f\n", (double)(end - start) / CLK_TCK);
+  cout << "abc" << endl;
 
   // Write the trajectories to file
   ofstream fout;
@@ -62,5 +85,3 @@ int main() {
 
   */
 }
-
-//hello
